60c41003ca32da4e9651cc7fd024f9e577e6b044
AmruthaVarshini06 <karriamruthavarshini6@gmail.com>
2026-04-04 10:30:08 +0530

    Deleted

--- JavaScript/Admin-Home.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("adminEmail").innerText =
-    localStorage.getItem("loggedInUser");
-
-function logout() {
-    localStorage.removeItem("loggedInUser");
-    localStorage.removeItem("role");
-    window.location.href = "Login.html";
-}
